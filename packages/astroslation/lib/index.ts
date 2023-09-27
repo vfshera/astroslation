88d3ea2be0b80c5c4124e4480e138a8c3f4097e4
Franklin Shera <fshera96@gmail.com>
2023-09-27 18:17:52 +0300

    create types file

--- packages/astroslation/lib/index.ts
+++ packages/astroslation/lib/index.ts
@@ -2,11 +2,15 @@ import type { AstroIntegration, AstroConfig } from "astro";
 import ViteRestart from "vite-plugin-restart";
 import type { i18nConfig } from "./types";
 import { PACKAGE_NAME, TRANSLATIONS_DIR } from "./constants";
+import { createFile, getPath } from "./utils";
+
+let translations = {};
 
 export default function createIntegration(
   userConfig: i18nConfig
 ): AstroIntegration {
   validateConfig(userConfig);
+
   /**
    * READ CONFIG
    *
@@ -20,18 +24,32 @@ export default function createIntegration(
   return {
     name: PACKAGE_NAME,
     hooks: {
-      "astro:config:setup": async ({
-        config,
-        addWatchFile,
-        updateConfig,
-        injectRoute,
-      }) => {
+      "astro:config:setup": async ({ config, updateConfig }) => {
         /**
          * Restart when translations change
          */
         const translationsGlob = new URL(
           `./src/${TRANSLATIONS_DIR}/**/*.json`,
           config.root
+        );
+
+        const declarationPath = getPath(
+          new URL("./src/i18n.d.ts", config.root)
+        );
+        console.log("Declaration Path: ", declarationPath);
+
+        const langKeys = Object.keys(userConfig.languages);
+
+        createFile(
+          declarationPath.__filename,
+          `
+export type Language = ${langKeys.map((key) => `"${key}"`).join(" | ")}
+export type i18nConfig = {
+  languages: Record<string, string>;
+  defaultLang: Language;
+};
+
+`
         );
 
         updateConfig({
@@ -44,21 +62,24 @@ export default function createIntegration(
             ],
           },
         });
-
-        injectRoute({
-          pattern: "/main",
-          entryPoint: getRedirectPage(config.output),
-        });
       },
     },
   };
 }
 
-function getRedirectPage(outputType: AstroConfig["output"]) {
-  return outputType === "server"
-    ? `${PACKAGE_NAME}/ssr.astro`
-    : `${PACKAGE_NAME}/static.astro`;
-}
+/**
+ * Retrieves the language code from the URL path ie. Astro.url
+ */
+//  export function getLangFromUrl(url: URL) {
+//   const [, lang] = url.pathname.split("/");
+//   if (lang in translations) return lang ;
+//   return defaultLang;
+// }
+
+/**
+ * Loads json translations
+ *  */
+// export const useTranslation = async (lang: Languages) => translations[lang]();
 
 function validateConfig(config: i18nConfig) {
   const { languages, defaultLang } = config;
