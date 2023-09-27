174bf85c997f955316e84f6be156c7ba18912efc
Franklin Shera <fshera96@gmail.com>
2023-09-27 18:16:53 +0300

    create file and path utils

--- packages/astroslation/lib/utils.ts
+++ packages/astroslation/lib/utils.ts
@@ -1,1 +1,19 @@
- 
+import fs from "fs";
+import { fileURLToPath } from "node:url";
+import path from "path";
+
+export function getPath(fileUrl: string | URL) {
+  const __filename = fileURLToPath(fileUrl);
+  const __dirname = path.dirname(__filename);
+
+  return { __dirname, __filename };
+}
+
+export function createFile(fileName: string, content: string) {
+  console.log(fileName);
+
+  fs.writeFile(fileName, content, (err) => {
+    if (err) throw err;
+    console.log(`${fileName} is created successfully.`);
+  });
+}
